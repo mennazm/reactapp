2f8c89db7822b94382c9bd5dd157eda5661c5cd4
fatma1-a7med <fatmaahmed110099@gmail.com>
2024-06-19 17:41:26 +0200

    update api

--- exam-system-frontend/src/Api/ExamApi.js
+++ exam-system-frontend/src/Api/ExamApi.js
@@ -9,9 +9,9 @@ const axiosInstance = axios.create({
 });
 axiosInstance.interceptors.request.use(
   (config) => {
-    const token = localStorage.getItem('token'); // Fetch the token from local storage
+    const token = localStorage.getItem('token'); 
     if (token) {
-      config.headers.Authorization = `Bearer ${token}`; // Attach the token to the Authorization header
+      config.headers.Authorization = `Bearer ${token}`; 
     }
     return config;
   },
@@ -37,41 +37,5 @@ export const loginUser = async (userData) => {
   }
 };
 
-export const getAllUsers = async () => {
-  try {
-    const response = await axiosInstance.get('/users');
-    return response.data;
-  } catch (error) {
-    throw error.response.data.message || 'Failed to fetch users';
-  }
-};
 
-export const getUserById = async (userId) => {
-  try {
-    const response = await axiosInstance.get(`/users/${userId}`);
-    return response.data;
-  } catch (error) {
-    throw error.response.data.message || 'Failed to fetch user';
-  }
-};
-
-export const updateUser = async (userId, userData) => {
-  try {
-    const response = await axiosInstance.put(`/users/${userId}`, userData);
-    return response.data;
-  } catch (error) {
-    throw error.response.data.message || 'Failed to update user';
-  }
-};
-
-export const deleteUser = async (userId) => {
-  try {
-    const response = await axiosInstance.delete(`/users/${userId}`);
-    return response.data;
-  } catch (error) {
-    throw error.response.data.message || 'Failed to delete user';
-  }
-};
-
-// Export the axios instance for advanced usage if needed
 export default axiosInstance;
